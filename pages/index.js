3d160195d05a9057d65b7af1c77627f60ef573fd
AbhinavOhri <abhinavohri13@gmail.com>
2023-06-20 19:25:09 +0530

    Added bar chart and sample data

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import BarChart from "@/components/BarChart";
 import { Header } from "@/components/Header";
+import RecentOrders from "@/components/RecentOrders";
+import TopCards from "@/components/TopCards";
 import Head from "next/head";
 
 export default function Home() {
@@ -12,6 +15,11 @@ export default function Home() {
       </Head>
       <main className="bg-gray-100 min-h-screen">
         <Header />
+        <TopCards />
+        <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
+          <BarChart />
+          <RecentOrders />
+        </div>
       </main>
     </>
   );
